830f0db98728e1c95f7f7c39edae076f01b4b584
Rahul Bilimoria <rahulbilimoria1@gmail.com>
2017-02-19 13:07:41 -0500

    added stuff to boxes

--- src/pokegame/entity/player/Storage.java
+++ src/pokegame/entity/player/Storage.java
@@ -13,50 +13,72 @@ import pokegame.utils.Utils;
  * @author Rahul
  */
 public class Storage {
-    
-    private static int BOXES_COUNT = 6;
-    private static int BOXES_SIZE = 30;
-    
+
+    private static int BOXES_COUNT = 7;
+    private static int BOXES_SIZE = 25;
+    private static int DATA_PER_POKEMON = 25;
+
     Player player;
     private int currentBox;
     private int selectedPokemon;
-    private int[][] boxes = new int[BOXES_COUNT][BOXES_SIZE];
-    private String[][] pokemonBoxes = new String[BOXES_COUNT][BOXES_SIZE];
-    private Pokemon[][] pokemon;
-    
-    public Storage(Player player){
-        this.player = player;
-        init();
-    }
-    
-    public void init(){
-        for (int x = 0; x < BOXES_COUNT; x++){
-            String p[] = Utils.loadFileAsString("dat/player/storage/box" + (x+1) + ".box").split("\n");
-            //String p[] = Utils.loadFileAsString("dat/player/storage/box" + (x+1) + ".box").split("\n")[0].split(" ");
-            //pokemonBoxes[x] = 
-            for (int y = 0; y < BOXES_SIZE; y++){
-                boxes[x][y] = Utils.parseInt(p[y]);
+    private PokemonBox[] pokemonBoxes = new PokemonBox[BOXES_COUNT];
+
+    class PokemonBox {
+
+        int id;
+        int box[][] = new int[BOXES_SIZE][25];
+
+        public PokemonBox(int id, String[] data) {
+            this.id = id;
+            parseData(data);
+        }
+
+        public int getPokemonId(int y) {
+            return box[y][0];
+        }
+
+        private void parseData(String[] data) {
+            for (int x = 0; x < BOXES_SIZE; x++) {
+                for (int y = 0; y < DATA_PER_POKEMON; y++) {
+                    box[x][y] = Utils.parseInt(data[y]);
+                }
             }
         }
     }
-    
-    public int getId(int x, int y){
-        return boxes[x][y];
+
+    public Storage(Player player) {
+        this.player = player;
+        init();
     }
-    
-    public void getPokemon(int x, int y){//need to return pokemon
-        
+
+    public void init() {
+        for (int x = 0; x < BOXES_COUNT; x++) {
+            String p[] = Utils.loadFileAsString("dat/player/storage/box" + (x + 1) + ".box").split("\n");
+            //String p[] = Utils.loadFileAsString("dat/player/storage/box" + (x+1) + ".box").split("\n")[0].split(" ");
+            //pokemonBoxes[x] = 
+            for (int y = 0; y < BOXES_SIZE; y++) {
+                pokemonBoxes[x] = new PokemonBox(x, p);
+            }
+        }
     }
-    
-    public void storePokemon(Pokemon p, int x, int y){//make pokemon.toStorage work
-        
+
+    public int getId(int x, int y) {
+        return pokemonBoxes[x].getPokemonId(y);
     }
-    
-    public int getCurrentBox(){
+
+    public void getPokemon(int x, int y) {//need to return pokemon
+
+    }
+
+    public void storePokemon(Pokemon p, int x, int y) {//make pokemon.toStorage work
+
+    }
+
+    public int getCurrentBox() {
         return currentBox;
     }
-    
-    public int getSelectedPokemon(){
+
+    public int getSelectedPokemon() {
         return selectedPokemon;
     }
 }
